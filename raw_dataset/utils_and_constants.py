fa1ab501fcb52072ad7ed395d6e5f070dd09f5d8
MonikaSonali <52571752+MonikaSonali@users.noreply.github.com>
2024-07-06 19:59:51 +0530

    Update utils_and_constants.py

--- raw_dataset/utils_and_constants.py
+++ raw_dataset/utils_and_constants.py
@@ -4,8 +4,8 @@ from pathlib import Path
 DATASET_TYPES = ["test", "train"]
 DROP_COLNAMES = ["Date"]
 TARGET_COLUMN = "RainTomorrow"
-RAW_DATASET = "raw_dataset/weather.csv"
-PROCESSED_DATASET = "processed_dataset/weather.csv"
+RAW_DATASET = "../raw_dataset/weather.csv"
+PROCESSED_DATASET = "../processed_dataset/weather.csv"
 
 
 def delete_and_recreate_dir(path):
